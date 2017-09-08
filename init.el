c52289f6b0d95bba16ccf5d119a7fb13af78adbd
Matt Stegall <mstegall928@gmail.com>
2017-09-08 08:05:17 -0400

    basic config

---
+++ init.el
@@ -0,0 +1,66 @@
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(setq package-enable-at-startup nil)
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; list the packages you want
+(setq package-list
+      '(
+        better-defaults
+        solarized-theme
+        ;;enable ido everywhere
+        ido-ubiquitous
+
+        ;; M-x autoscomplete
+        smex
+        
+        ;;better package search
+        paradox
+
+        ;;task manager
+        org))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(require 'better-defaults)
+(load-theme 'solarized-dark t)
+
+(setq inhibit-splash-screen t
+      initial-scratch-message nil
+      initial-major-mode 'org-mode)
+
+;; Show line numbers
+(global-linum-mode)
+
+;; Set font settings
+(set-face-attribute `default nil
+                    :family "Fira Code"
+                    :height 140)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (org better-defaults)))
+ '(paradox-github-token t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
