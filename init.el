f69cfc5add8ec4f50d1113cc2d6d97c98141d34f
Matt Stegall <mstegall928@gmail.com>
2017-09-09 08:13:26 -0400

    Add more js tools

--- init.el
+++ init.el
@@ -19,8 +19,10 @@
         better-defaults
         solarized-theme
 
-        ;;language packages
-        js2-mode
+        ;; javascript packages
+        js2-mode ;; major mode
+        js2-refactor ;; refactor support
+        xref-js2 ;; find references
         
         ;;enable ido everywhere
         ido-ubiquitous
@@ -42,11 +44,26 @@
 (require 'better-defaults)
 (load-theme 'solarized-dark t)
 
+;; Javascript config
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 ;; Better imenu
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+
+(require 'js2-refactor)
+(require 'xref-js2)
+
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+;; unbind it.
+(define-key js-mode-map (kbd "M-.") nil)
+
+(add-hook 'js2-mode-hook (lambda ()
+  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 
 (setq inhibit-splash-screen t
       initial-scratch-message nil
